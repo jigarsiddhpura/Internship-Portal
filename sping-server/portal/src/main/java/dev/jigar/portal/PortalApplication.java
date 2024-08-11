655cba2de25655a3214d3984837f0f713f1450b0
Jigar <95428432+jigarsiddhpura@users.noreply.github.com>
2024-08-11 14:29:04 +0530

    init

---
+++ sping-server/portal/src/main/java/dev/jigar/portal/PortalApplication.java
@@ -0,0 +1,14 @@
+package dev.jigar.portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+public class PortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortalApplication.class, args);
+	}
+
+}
---
+++ sping-server/portal/src/test/java/dev/jigar/portal/PortalApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.jigar.portal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
