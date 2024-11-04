d1272791165c1d27cc8e479428feb884c4bafe47
Jigar <95428432+jigarsiddhpura@users.noreply.github.com>
2024-11-04 16:04:21 +0530

    refactor: add `docker` for local postgres db

--- server/users/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Internship,Research_Project, Courses
-
-# Register your models here.
-admin.site.register(Internship)
-admin.site.register(Research_Project)
-admin.site.register(Courses)
